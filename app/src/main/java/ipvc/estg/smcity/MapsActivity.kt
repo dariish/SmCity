909f631ca6dbc6a39735ee30725ca6b9498df6a3
Tiago Santos <darish26@hotmail.com>
2021-05-06 14:28:23 +0100

    Know Location, com camara Zoom

--- app/src/main/java/ipvc/estg/smcity/MapsActivity.kt
+++ app/src/main/java/ipvc/estg/smcity/MapsActivity.kt
@@ -1,8 +1,14 @@
 package ipvc.estg.smcity
 
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,7 +18,6 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import ipvc.estg.smcity.api.EndPoints
 import ipvc.estg.smcity.api.Ocorrencia
-import ipvc.estg.smcity.api.OutputLogin
 import ipvc.estg.smcity.api.ServiceBuilder
 import retrofit2.Call
 import retrofit2.Callback
@@ -23,6 +28,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var ocorrencias: List<Ocorrencia>
 
+    //Last Known Location
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -32,7 +41,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-
+        //Iniciar o fusedLocationClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         val request = ServiceBuilder.buildService(EndPoints::class.java)
         val call = request.getOcorrencias()
@@ -60,9 +70,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        setUpMap()
         // Add a marker in Sydney and move the camera
       //  val sydney = LatLng(-34.0, 151.0)
        // mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    fun setUpMap(){
+        if(ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 11)
+
+            return
+        }else{
+            mMap.isMyLocationEnabled = true
+
+            fusedLocationClient.lastLocation.addOnSuccessListener(this) {location ->
+                if(location != null){
+                    lastLocation = location
+                    Toast.makeText(this@MapsActivity, lastLocation.toString(), Toast.LENGTH_SHORT).show()
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                }
+            }
+        }
     }
 }
